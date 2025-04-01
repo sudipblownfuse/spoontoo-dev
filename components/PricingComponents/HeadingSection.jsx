66e89c6d19a0d7151ffec84d1811468f6f7cef65
sudipblownfuse <sudip.g@blownfuse.io>
2025-04-01 10:56:47 +0530

    added pricing page

--- components/PricingComponents/HeadingSection.jsx
+++ components/PricingComponents/HeadingSection.jsx
@@ -1,16 +1,32 @@
 "use client";
 
+import { motion } from "framer-motion";
 
 const HeadingSection = () => {
   return (
     <section className="w-full h-auto pt-16">
       <div className="w-full width-container mx-auto px-5 flex flex-col items-center">
-        <h1 className="text-4xl font-semibold text-center">Pricing Table</h1>
-        <h6 className="w-[70%] py-6 text-center">
+        <motion.h1
+          className="text-4xl font-semibold text-center"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          viewport={{ once: true }}
+        >
+          Pricing Table
+        </motion.h1>
+
+        <motion.h6
+          className="w-[70%] py-6 text-center"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
+          viewport={{ once: true }}
+        >
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since.
-        </h6>
+        </motion.h6>
       </div>
     </section>
   );
--- components/PricingComponents/PricingSection.jsx
+++ components/PricingComponents/PricingSection.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from "react";
 import PricingCards from "./PricingCards";
+import { motion } from "framer-motion";
 
 const PricingSection = () => {
   const [isYearly, setIsYearly] = useState(true);
@@ -10,7 +11,13 @@ const PricingSection = () => {
     <section className="w-full h-auto pt-6">
       <div className="w-full width-container mx-auto px-14 flex flex-col items-center">
         {/* Toggler Button */}
-        <div className=" relative flex items-center justify-center">
+        <motion.div 
+        className=" relative flex items-center justify-center"
+        initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5, ease: "easeOut" }}
+          viewport={{ once: true }}
+          >
           <h5 className={`px-4 py-2 text-xl font-semibold`}>Monthly</h5>
           <div
             className={`relative w-12 h-6 flex items-center rounded-full mx-2 cursor-pointer drop-shadow-2xlshadow-4xl ${
@@ -31,7 +38,7 @@ const PricingSection = () => {
               Save 40% yearly
             </h6>
           </div>
-        </div>
+        </motion.div>
       </div>
 
       <PricingCards isYearly={isYearly}/>
