49871e6a5cfe409e4046b057b0e623f749034c06
sudipblownfuse <sudip.g@blownfuse.io>
2025-04-11 16:12:07 +0530

    update info section

--- components/CakeArtistComponents/CakeArtistInfo.jsx
+++ components/CakeArtistComponents/CakeArtistInfo.jsx
@@ -35,13 +35,13 @@ const CakeArtistInfo = () => {
 
   return (
     <section className="w-full h-auto bg-black">
-      <div className="w-full width-container mx-auto px-5 sm:px-14 flex justify-between flex-wrap py-10">
+      <div className="w-full width-container mx-auto px-5 sm:px-14 flex justify-between flex-wrap py-16 lg:py-10">
         {data.map((item, index) => (
           <div
             key={index}
             className="w-[44%] lg:w-[24%] flex flex-col items-center justify-center mb-8 lg:mb-0"
           >
-            <Image src={item.img} alt={item.heading} />
+            <Image className="w-10 sm:w-auto" src={item.img} alt={item.heading} />
             {(() => {
               const ref = useRef(null);
               const isInView = useInView(ref, { once: true });
@@ -71,13 +71,13 @@ const CakeArtistInfo = () => {
               }, [isInView]);
 
               return (
-                <h4 ref={ref} className="text-white font-bold text-2xl lg:text-3xl py-2">
+                <h4 ref={ref} className="text-white font-bold text-xl sm:text-2xl lg:text-3xl py-2">
                   {index === 0 ? `${count} Hours` : `${count} +`}
                 </h4>
               );
             })()}
 
-            <h6 className=" text-gray-300 text-sm sm:text-base">
+            <h6 className=" text-gray-300 text-xs sm:text-sm md:text-base">
               {item.heading}
             </h6>
           </div>
